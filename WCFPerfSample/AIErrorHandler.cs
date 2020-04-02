614829dc2ec78abc5f066f15414bfb8df8905eec
Andy Wahrenberger <andywahr@microsoft.com>
2020-04-02 12:34:35 -0500

    Add project files.

---
+++ WCFPerfSample/AIErrorHandler.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.ApplicationInsights;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Configuration;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Web;
+
+namespace WCFPerfSample
+{
+    public class AIErrorHandler : IErrorHandler
+    {
+        private TelemetryClient telemetry = new TelemetryClient();
+
+        public bool HandleError(Exception error)
+        {
+            telemetry.TrackException(error);
+            return false;
+        }
+
+        public void ProvideFault(Exception error, MessageVersion version, ref Message fault)
+        {
+
+        }
+    }
+
+    public class AIErrorHandlerBehavior : BehaviorExtensionElement, IServiceBehavior 
+    {
+        public override Type BehaviorType => typeof(AIErrorHandlerBehavior);
+
+        public void AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters)
+        {
+        }
+
+        public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+            var errorHandler = new AIErrorHandler();
+            foreach (ChannelDispatcher chanDisp in serviceHostBase.ChannelDispatchers)
+            {
+                chanDisp.ErrorHandlers.Add(errorHandler);
+            }
+        }
+
+        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+        }
+
+        protected override object CreateBehavior()
+        {
+            return new AIErrorHandlerBehavior();
+        }
+    }
+}
---
+++ WCFPerfSample/Service1.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WCFPerfSample
+{
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            throw new NullReferenceException("bob");
+            using (wcftestEntities dbContent = new wcftestEntities())
+            {
+                var orders = dbContent.SalesOrderHeaders.Where(oh => oh.SalesOrderID > 70000)
+                    .Include(oh => oh.SalesOrderDetails.Select(od => od.Product))
+                    .Include(oh => oh.Address)
+                    .Include(oh => oh.Customer)
+                    .Include(oh => oh.SalesOrderDetails).ToList();
+
+                return orders.First().Customer.LastName;
+            }
+        }
+
+        public async Task<string> AsyncGetData(int value)
+        {
+            using (wcftestEntities dbContent = new wcftestEntities())
+            {
+                var orders = await dbContent.SalesOrderHeaders.Where(oh => oh.SalesOrderID > 70000)
+                    .Include(oh => oh.SalesOrderDetails.Select(od => od.Product))
+                    .Include(oh => oh.Address)
+                    .Include(oh => oh.Customer)
+                    .Include(oh => oh.SalesOrderDetails).ToListAsync();
+
+                return orders.First().Customer.LastName;
+            }
+        }
+    }
+}
